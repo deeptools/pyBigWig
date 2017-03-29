ed2da2ff4f5ddbc602a7cc870552fa554d6f135d
dpryan79 <dpryan79@gmail.com>
2017-03-29 17:02:53 +0200

    Bump version to 0.3.4

--- setup.py
+++ setup.py
@@ -58,7 +58,7 @@ module1 = Extension('pyBigWig',
                     include_dirs = include_dirs)
 
 setup(name = 'pyBigWig',
-       version = '0.3.3',
+       version = '0.3.4',
        description = 'A package for accessing bigWig files using libBigWig',
        author = "Devon P. Ryan",
        author_email = "ryan@ie-freiburg.mpg.de",
